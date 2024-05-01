efa0ea6cead5999fd05a4f205bb55c037246da66
Stanley Ke <kestanley101@gmail.com>
2024-04-30 20:53:45 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var buttonClicks = 0;
     var button = document.getElementById("clickButton");
+    var body = document.body;
     
     button.addEventListener("click", handleClick);
     function handleClick() {
@@ -8,20 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
         var oldText = document.getElementById("hiddenMessage");
         console.log("Button clicked " + buttonClicks + " times.");
         if (buttonClicks === 3) {
-            oldText.textContent = "."
+            body.style.backgroundColor = "gray";
+            oldText.textContent = ".";
         }
         if (buttonClicks === 5) {
-            oldText.textContent = "Ouch."
+            oldText.textContent = "Ouch.";
         }
         if (buttonClicks === 10) {
-            oldText.textContent = "OMO"
+            oldText.textContent = "OMO";
         }
         if (buttonClicks > 10) {
             oldText.textContent += "O";
         }
         if (buttonClicks === 100) {
             button.removeEventListener("click", handleClick);
-            oldText.textContent = "ONO the end."
+            oldText.textContent = "ONO the end.";
         }
     };
 
